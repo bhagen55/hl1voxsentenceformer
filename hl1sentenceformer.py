3d7862be5af56f84e232ca47033cd6a041eceb59
Blair Hagen <hagenb@union.edu>
2018-07-29 16:51:04 -0700

    Removed audio playing functionality

--- hl1sentenceformer.py
+++ hl1sentenceformer.py
@@ -8,7 +8,7 @@
 #	- bizwarn
 #	- buzwarn
 import sys
-from playsound import playsound
+# from playsound import playsound
 from pydub import AudioSegment
 from slugify import slugify
 import os
@@ -23,66 +23,71 @@ ing = "ing"
 
 
 # Takes sentence and seperates into individual words
+# converts commas and periods into their respective word
 def convertsentence(sentence):
-    words = sentence.split()
-    output = []
+	words = sentence.split()
+	output = []
 
-    for word in words:
-        # if word[-3:] == 'ing':
-        # 	output.append(word[:-3])
-        # 	output.append(ing)
-        if word[-1:] == ",":
-            output.append(word[:-1])
-            output.append(comma)
-        elif word[-1:] == ".":
-            output.append(word[:-1])
-            output.append(period)
-        else:
-            output.append(word)
-    return output
-
-
-# Play sentence
-def playwords(sentence):
-    words = convertsentence(sentence)
-    for word in words:
-        play(word)
+	for word in words:
+		# if word[-3:] == 'ing':
+		# 	output.append(word[:-3])
+		# 	output.append(ing)
+		if word[-1:] == ",":
+			output.append(word[:-1])
+			output.append(comma)
+		elif word[-1:] == ".":
+			output.append(word[:-1])
+			output.append(period)
+		else:
+			output.append(word)
+	return output
 
 
-# Wrapper to add path and filetype
-def play(word):
-    playsound(soundpath + word + filetype)
+# # Play sentence
+# def playwords(sentence):
+#     words = convertsentence(sentence)
+#     for word in words:
+#         play(word)
 
 
-# Save sentence as an mp3
+# # Wrapper to add path and filetype
+# def play(word):
+#     playsound(soundpath + word + filetype)
+
+
+# Save sentence as an mp3 with the final text included
 def savetomp3(sentence):
-    words = convertsentence(sentence)
-    sentence = " ".join(words)
-    if os.path.isfile(combined_path + slugify(sentence) + ".mp3"):
-        print("Sentence already in cache, not re-creating")
-        return(combined_path + slugify(sentence) + ".mp3")
-    else:
-        words = convertsentence(sentence)
-        playlist = AudioSegment.silent(duration=500)
-        for word in words:
-            if not os.path.isfile(soundpath + word + filetype):
-                print(word + " does not exist, skipping")
-                sentence = sentence.replace(word, '')
-            else:
-                word_mp3 = AudioSegment.from_mp3(soundpath + word + filetype)
-                playlist = playlist.append(word_mp3)
-                playlist = playlist.append(AudioSegment.silent(duration=300))
-        if not playlist:
-            print("Cannot say any of the sentence")
-            return None
-        else:
-            playlist.export(combined_path + slugify(sentence) + ".mp3", format="mp3", bitrate="100k")
-            return(combined_path + slugify(sentence) + ".mp3")
+	words = convertsentence(sentence)
+	sentence = " ".join(words)
+	if os.path.isfile(combined_path + slugify(sentence) + ".mp3"):
+		print("Sentence already in cache, not re-creating")
+		return(combined_path + slugify(sentence) + ".mp3")
+	elif os.path.isfile(soundpath + slugify(sentence) + ".mp3"):
+		print("Sentence is a static word, not adding to cache")
+		return(soundpath + slugify(sentence) + ".mp3")
+	else:
+		words = convertsentence(sentence)
+		playlist = AudioSegment.silent(duration=500)
+		for word in words:
+			if not os.path.isfile(soundpath + word + filetype):
+				print(word + " does not exist, skipping")
+				sentence = sentence.replace(word, '')
+			else:
+				word_mp3 = AudioSegment.from_mp3(soundpath + word + filetype)
+				playlist = playlist.append(word_mp3)
+				playlist = playlist.append(AudioSegment.silent(duration=300))
+		if not playlist:
+			print("Cannot say any of the sentence")
+			return None
+		else:
+			playlist.export(combined_path + slugify(sentence) + ".mp3", format="mp3", bitrate="100k")
+			path = combined_path + slugify(sentence) + ".mp3"
+			return {'path': combined_path, 'sentence': act_sentence}
 
-def main():
-    saystring = sys.argv[1];
-    # playwords(saystring)
-    savetomp3(saystring)
+# def main():
+#     saystring = sys.argv[1];
+#     # playwords(saystring)
+#     savetomp3(saystring)
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
